4788fe8c76a6e716e936ba375e9323ee6350f78d
unknown <maretanur197@gmail.com>
2023-12-19 10:37:49 +0700

    add: project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    colors: {
+      orange: "#FF860D",
+      orange2: "#EDB059",
+    },
+  },
+  plugins: [],
+};
+
